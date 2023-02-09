62439ae51f044323adbfb08b4f6826bdf1c5d70e
macs-massimopiazza <me@massimopiazza.com>
2023-02-09 17:38:39 +0100

    format collectors list as table

--- resources/views/pages/collectors/index.blade.php
+++ resources/views/pages/collectors/index.blade.php
@@ -3,13 +3,25 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="off-set-3 col-6">
-                <ul>
+            <table class="table">
+                <thead>
+                  <tr>
+                      <th scope="col">Image</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Collectors N° of Cars</th>
+                  </tr>
+                </thead>
+                <tbody>
                     @foreach ($collectors as $collector)
-                        <li>{{ $collector->first_name }} <img src="{{ $collector->path_img }}" alt="{{ $collector->first_name }}"></li>
+                        <tr>
+                            <th scope="row"><img src="{{ $collector->path_img }}" alt="{{ $collector->first_name }}"></th>
+                            <td >{{ $collector->first_name }}</td>
+                            <td>{{ $collector->cars_num}}</td>
+                        </tr>
                     @endforeach
-                </ul>
-            </div>
+                  
+                </tbody>
+              </table>
         </div>
     </div>
 @endsection
