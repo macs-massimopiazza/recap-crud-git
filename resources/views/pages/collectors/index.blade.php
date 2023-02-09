959e6ee7a115fcb3f4a814cd2fe1059318c45cc6
macs-massimopiazza <me@massimopiazza.com>
2023-02-09 17:00:35 +0100

    feat: show collectors in index view

---
+++ resources/views/pages/collectors/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Collectors</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="off-set-3 col-6">
+                @dd($collectors)
+            </div>
+        </div>
+    </div>
+</body>
+</html>
