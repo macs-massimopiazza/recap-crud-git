fd9293bd024212c82d7cf20ee5b855c90cb96b85
macs-massimopiazza <me@massimopiazza.com>
2023-02-09 17:26:02 +0100

    feat: show list of collectors in index view

--- resources/views/pages/collectors/index.blade.php
+++ resources/views/pages/collectors/index.blade.php
@@ -11,7 +11,11 @@
     <div class="container">
         <div class="row">
             <div class="off-set-3 col-6">
-                @dd($collectors)
+                <ul>
+                    @foreach ($collectors as $collector)
+                        <li>{{ $collector->first_name }} <img src="{{ $collector->path_img }}" alt="{{ $collector->first_name }}"></li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
